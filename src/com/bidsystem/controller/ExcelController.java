6b66eb0b5c70858994895c973a2a4899cb5df3f4
xuewei <2827577486@qq.com>
2018-12-20 13:39:41 +0800

    test

---
+++ src/com/bidsystem/controller/ExcelController.java
@@ -0,0 +1,10 @@
+package com.bidsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("excel")
+public class ExcelController {
+
+}
